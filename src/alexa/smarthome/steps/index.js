41d99741b81e058d2c0902505de04ab338288ff5
Brian Rogers <rogers.brian@gmail.com>
2018-04-06 09:28:11 -0700

    Removing ';' per standards https://standardjs.com/rules.html

--- src/alexa/smarthome/steps/index.js
+++ src/alexa/smarthome/steps/index.js
@@ -32,29 +32,29 @@ function buildReportState(req, stateValue) {
 }
 
 function log(message, message1, message2) {
-    console.log(message + message1 + message2);
+    console.log(message + message1 + message2)
 }
 
 exports.handler = function (request, context) {
     if (request.directive.header.namespace === 'Alexa.Discovery' && request.directive.header.name === 'Discover') {
-        log('DEBUG:', 'Discover request',  JSON.stringify(request));
-        handleDiscovery(request, context, '');
+        log('DEBUG:', 'Discover request',  JSON.stringify(request))
+        handleDiscovery(request, context, '')
     }
     else if (request.directive.header.namespace === 'Alexa.PowerController') {
         if (request.directive.header.name === 'TurnOn' || request.directive.header.name === 'TurnOff') {
-            log('DEBUG:', 'TurnOn or TurnOff Request', JSON.stringify(request));
-            handlePowerControl(request, context);
+            log('DEBUG:', 'TurnOn or TurnOff Request', JSON.stringify(request))
+            handlePowerControl(request, context)
         }
     } else if (request.directive.header.namespace === 'Alexa') {
         // This is required to allow Alexa to know the current state of the device
         // It keeps the message 'Server is unreponsive.' from being displayed
         if (request.directive.header.name === 'ReportState') {
-            log('DEBUG:', 'ReportState Request ', JSON.stringify(request));
-            handleReportState(request, context);
+            log('DEBUG:', 'ReportState Request ', JSON.stringify(request))
+            handleReportState(request, context)
         }
     }
     else {
-        log('DEBUG:', 'Unknown request',  JSON.stringify(request));
+        log('DEBUG:', 'Unknown request',  JSON.stringify(request))
     }
 
     function handleDiscovery(request, context) {
@@ -94,31 +94,32 @@ exports.handler = function (request, context) {
                     ]
                 }
             ]
-        };
-        var header = request.directive.header;
-        header.name = 'Discover.Response';
-        log('DEBUG', 'Discovery Response: ', JSON.stringify({ header: header, payload: payload }));
-        context.succeed({ event: { header: header, payload: payload } });
+        }
+        var header = request.directive.header
+        header.name = 'Discover.Response'
+        log('DEBUG', 'Discovery Response: ', JSON.stringify({ header: header, payload: payload }))
+        context.succeed({ event: { header: header, payload: payload } })
     }
 
     function handlePowerControl(request, context) {
         // get device ID passed in during discovery
-        var requestMethod = request.directive.header.name;
+        var requestMethod = request.directive.header.name
         // get user token pass in request
-        var requestToken = request.directive.endpoint.scope.token;
-        var powerResult;
+        var requestToken = request.directive.endpoint.scope.token
+        var powerResult
 
         if (requestMethod === 'TurnOn') {
 
             // Make the call to your device cloud for control 
-            // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-            powerResult = 'ON';
+            // powerResult = stubControlFunctionToYourCloud(endpointId, token, request)
+            powerResult = 'ON'
         }
        else if (requestMethod === 'TurnOff') {
             // Make the call to your device cloud for control and check for success 
-            // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-            powerResult = 'OFF';
+            // powerResult = stubControlFunctionToYourCloud(endpointId, token, request)
+            powerResult = 'OFF'
         }
+
         var contextResult = {
             properties: [{
                 namespace: 'Alexa.PowerController',
@@ -127,7 +128,8 @@ exports.handler = function (request, context) {
                 timeOfSample: '2017-09-03T16:20:50.52Z', //retrieve from result.
                 uncertaintyInMilliseconds: 50
             }]
-        };
+        }
+
         var response = {
             context: contextResult,
             event: {
@@ -140,12 +142,12 @@ exports.handler = function (request, context) {
                 payload: {}
             },
             endpoint: request.directive.endpoint
-        };
-        log('DEBUG', 'Alexa.PowerController ', JSON.stringify(response));
-        context.succeed(response);
+        }
+        log('DEBUG', 'Alexa.PowerController ', JSON.stringify(response))
+        context.succeed(response)
     }
 
     function handleReportState(request, context) {
         context.succeed(buildReportState(request, 'OFF'))
     }
-};
+}
