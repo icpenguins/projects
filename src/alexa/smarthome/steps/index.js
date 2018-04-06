2b0925bf258303db425861f5194031978f46cd08
Brian Rogers <rogers.brian@gmail.com>
2018-04-06 10:07:11 -0700

    Comment out unused variable

--- src/alexa/smarthome/steps/index.js
+++ src/alexa/smarthome/steps/index.js
@@ -79,7 +79,7 @@ function handlePowerControl(request, context) {
     // get device ID passed in during discovery
     var requestMethod = request.directive.header.name
     // get user token pass in request
-    var requestToken = request.directive.endpoint.scope.token
+    // var requestToken = request.directive.endpoint.scope.token
     var powerResult
 
     if (requestMethod === 'TurnOn') {
